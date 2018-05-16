01fac9ba69b137c17ea5cbfe210749528668354e
minhquangpm <dalv03@gmail.com>
2018-05-16 21:01:17 +0700

    quangpm
    add js file

---
+++ javascript/brand.js
@@ -0,0 +1,6 @@
+AOS.init();
+$(function() {
+	$(".show-expand-menu").click(function(){
+		$(".expand-menu").toggle();
+	})
+});
---
+++ javascript/header-slide.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+      var FixNavBar = $('.nav-bar').offset().top;
+      var FixLogo = $('.logo').offset().top;
+      var FixBagItem = $('.bag-item').offset().top;
+      var stickyNav = function(){
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > FixNavBar) {
+                  $('.sub-menu').addClass('fix-submenu');
+                  $('.nav-bar').addClass('sticky');
+                  $('.logo').addClass('hide');
+                  $('.bag-item').addClass('fix-bag');
+            }
+            else {
+                  $('.sub-menu').removeClass('fix-submenu');
+                  $('.nav-bar').removeClass('sticky');
+                  $('.logo').removeClass('hide');
+                  $('.bag-item').removeClass('fix-bag');
+            }
+      };
+      $(window).scroll(function() {
+            stickyNav();
+      });
+});
