6c9cd4afd12b1ee41de56eff8920fd70a3387c4b
minhquangpm <dalv03@gmail.com>
2018-05-18 17:01:34 +0700

    quangpm
    fix header.js

--- javascript/header.js
+++ javascript/header.js
@@ -1,32 +1,45 @@
 $(document).ready(function() {
-		    var FixNavBar = $('.nav-bar').offset().top;
-		    var FixLogo = $('.logo').offset().top;
-		    var FixBagItem = $('.bag-item').offset().top;
-		    var stickyNav = function(){
-		    var scrollTop = $(window).scrollTop();
-		        if (scrollTop > FixNavBar) {
-		            $('.sub-menu').addClass('fix-submenu');
-		            $('.nav-bar').addClass('sticky');
-		            $('.logo').addClass('hide');
-		            $('.bag-item').addClass('fix-bag');
-		        }
-		        else {
-		            $('.sub-menu').removeClass('fix-submenu');
-		            $('.nav-bar').removeClass('sticky');
-		            $('.logo').removeClass('hide');
-		            $('.bag-item').removeClass('fix-bag');
-		        }
-		    };
-		    $(window).scroll(function() {
-		        stickyNav();
-		    }); 
-$(document).ready(function(){
-		    $('.fa-bars').click(function(){
-		        $('.sub-menu').toggleClass('active-menu');
-		    });
-		});
-		$(document).ready(function(){
-		    $('.brand-dropdown').click(function(){
-		        $('.drop-menu').toggleClass('active-dropdown');
-		    });
-		});
+      var FixNavBar = $('.nav-bar').offset().top;
+      var FixLogo = $('.logo').offset().top;
+      var FixBagItem = $('.bag-item').offset().top;
+      var stickyNav = function(){
+      var scrollTop = $(window).scrollTop();
+            if (scrollTop > FixNavBar) {
+                  $('.sub-menu').addClass('fix-submenu');
+                  $('.nav-bar').addClass('sticky');
+                  $('.logo').addClass('hide');
+                  $('.bag-item').addClass('fix-bag');
+            }
+            else {
+                  $('.sub-menu').removeClass('fix-submenu');
+                  $('.nav-bar').removeClass('sticky');
+                  $('.logo').removeClass('hide');
+                  $('.bag-item').removeClass('fix-bag');
+            }
+      };
+      $(window).scroll(function() {
+            stickyNav();
+      });
+      $('.bathroom').click(function(){
+            window.location.href = 'https://www.google.com.vn';
+      });     
+      $('.living-room').click(function(){
+            window.location.href = 'https://www.google.com.vn';
+      });
+      $('.dining-room').click(function(){
+            window.location.href = 'https://www.google.com.vn';
+      });
+      $('.bed-room').click(function(){
+            window.location.href = 'https://www.google.com.vn';
+      });
+      $('.office').click(function(){
+            window.location.href = 'https://www.google.com.vn';
+      });     
+      $('.fa-bars').click(function(){
+            $('.sub-menu').toggleClass('active-menu');
+      });
+      $('.brand-dropdown').click(function(){
+            $('.drop-menu').toggleClass('active-dropdown');
+      });
+});
+        
