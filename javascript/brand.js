1e700bb8fc7383e1ae809428580014d1cb5ed9e7
minhquangpm <dalv03@gmail.com>
2018-05-18 16:47:01 +0700

    quangpm
    update brand page

--- javascript/brand.js
+++ javascript/brand.js
@@ -1,5 +1,18 @@
 $(function() {
 	AOS.init();
+	var fixMenuLeft = $(".menu-left").offset().top;
+	// var fixNavBar = $(".nav-bar").offset().top;
+	var stickyLeftMenu = function() {
+		var scrollTop = $(window).scrollTop();
+		if (scrollTop + 85 > fixMenuLeft) {
+			$(".menu-left").addClass("fix-menu-left");
+		} else {
+			$(".menu-left").removeClass("fix-menu-left");
+		}
+	};
+	$(window).scroll(function() {
+            stickyLeftMenu();
+    });
 	$(".show-expand-menu").click(function(){
 		$(".expand-menu").toggle();
 	})
