f200f677cc47630c26b27478e85d0ca7531c82a3
Your Name <you@example.com>
2018-05-27 19:53:15 +0700

    quangpm
    update ctsp

---
+++ javascript/add-to-cart.js
@@ -0,0 +1,81 @@
+$(function() {
+	showCartIfExists();
+	$('.addToCart').click(function() {
+		updateCart();
+		var pCount = 1;
+		var pID = $(this).parents('.pro-item').attr('data-id');
+		var pTitle = $(this).parents('.pro-item').attr('data-title');
+		var pPrice = $(this).parents('.pro-item').attr('data-price');
+		var pImage = $(this).parents('.pro-item').find('img').attr('src').split('image')[1];
+		// console.log(pImage);
+		if (localStorage.getItem('cart')) {
+			var pCart = JSON.parse(localStorage.getItem('cart'));
+			if (pCart && pCart[pID]) {
+				pCount = pCart[pID].count + pCount;
+				// console.log(pID);
+				var pData = {[pID]: {"title": pTitle, "price": pPrice, "image": pImage, "count": pCount}};
+				// extend pData to pCart
+				$.extend(pCart, pData);
+				var pCart = JSON.stringify(pCart);
+				// console.log(pCart);
+			} else {
+				var pData = {[pID]: {"title": pTitle, "price": pPrice, "image": pImage, "count": pCount}};
+				$.extend(pCart, pData);
+				var pCart = JSON.stringify(pCart);
+				// console.log(pCart);
+			}
+		} else {
+			var pCart = '{"' + pID + '" : {"title":"' + pTitle + '", "price":"' + pPrice + '", "image":"' + pImage + '", "count":' + pCount + '}}';
+		}
+		localStorage.setItem('cart', pCart);
+	});
+
+	function showCartIfExists() {
+		//show counter if this is the first item added to the cart
+		var cart = $('.bag-item');
+		// ( !cart.hasClass('items-added') ) && cart.addClass('items-added'); 
+		var cartItems = cart.find('span');
+		// check localStorage for total items added
+		if (!cart.hasClass('items-added')) {
+			var pCart = localStorage.getItem('cart');
+			if (pCart) {
+				pCart = JSON.parse(pCart);
+				var countList = [],
+					sum = 0;
+				$.each(pCart, function (key, data) {
+				    var count = pCart[key].count;
+				    countList.push(count);
+				});
+				for (var i = 0; i < countList.length; i++) {
+		    		sum += countList[i] << 0;
+				}
+				cartItems.text(sum);
+				cart.addClass('items-added');
+			}
+		}
+	};
+
+	function updateCart() {
+		var cart = $('.bag-item');
+		var cartItems = cart.find('span');
+		var pCart = localStorage.getItem('cart');
+		if (pCart) {
+			pCart = JSON.parse(pCart);
+			var countList = [],
+				sum = 1;
+			$.each(pCart, function (key, data) {
+			    var count = pCart[key].count;
+			    countList.push(count);
+			});
+			for (var i = 0; i < countList.length; i++) {
+	    		sum += countList[i];
+			}
+			cartItems.text(sum);
+			cart.addClass('items-added');
+		} else {
+			var text = parseInt(cartItems.text()) + 1;
+			cartItems.text(text);
+			cart.addClass('items-added');
+		}
+	}
+});
---
+++ javascript/cart-on-load.js
@@ -0,0 +1,40 @@
+$(function() {
+	var row = $('.append-cart');
+	var pCart = localStorage.getItem('cart');
+	pCart = JSON.parse(pCart);
+	$.each(pCart, function (key, data) {
+		var title = pCart[key].title;
+		var price = parseInt(pCart[key].price.split('vnd')[0]);
+		var image = pCart[key].image;
+		var count = pCart[key].count;
+	    row.append('<tr>'
+	    	+ '<td><img class="resize-img" src="../image' + image + '"></td>'
+	    	+ '<td>' + key + '</td>'
+	    	+ '<td>' + title + '</td>'
+	    	+ '<td>' + price + 'vnd</td>'
+	    	+ '<td>' + count + '</td>'
+	    	+ '<td>' + price * count + 'vnd</td>'
+	    	// + '<td><button class="btn">Pay</button></td>'
+	    	+ '<td><button id="' + key + '" class="btn red-bg removeCartItem">Remove</button></td>'
+	    	+ '</tr>')
+	});
+
+	$('.removeCartItem').click(function() {
+		var id = $(this).attr('id');
+		var pCart = localStorage.getItem('cart');
+		pCart = JSON.parse(pCart);
+		delete pCart[id];
+		console.log(pCart);
+		if (jQuery.isEmptyObject(pCart)) {
+			localStorage.clear();
+		} else {
+			pCart = JSON.stringify(pCart);
+			localStorage.setItem('cart', pCart);
+		};
+		$(this).parents('tr').remove();
+	});
+
+	$('.back-shop').click(function() {
+		window.location.reload();
+	})
+})
